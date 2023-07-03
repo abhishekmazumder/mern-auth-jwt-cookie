796e645bbd97a88ae2a7a5572b415e7bbb89a8fa
Abhishek Mazumder <abhishekmazumder69@gmail.com>
2023-07-03 20:24:03 +0530

    User model created

---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // to add created_at and updated_at fields in the schema by default
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
