7d1cce08347b1b1d61d0c088426dacc261fb3788
Abhishek Mazumder <abhishekmazumder69@gmail.com>
2023-07-04 00:49:11 +0530

    User Login, Registration, Authentication, Update api completed

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema(
   {
@@ -20,6 +21,18 @@ const userSchema = mongoose.Schema(
     timestamps: true, // to add created_at and updated_at fields in the schema by default
   }
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
---
+++ backend/utils/generateToken.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: '1h', //expires in one hour
+  });
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'strict',
+    maxAge: 24*60*60,
+  });
+};
+
+export default generateToken;
